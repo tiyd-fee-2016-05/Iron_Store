476d8b9e2f8e4013f162af91f7092977d2d3b721
MARY BETH CLARK <marybeth.clark@icloud.com>
2016-07-03 13:15:44 -0400

    add ineffective code trying to show product details  when item image clicked

--- assets/js/productmaincontroller.js
+++ assets/js/productmaincontroller.js
@@ -13,6 +13,7 @@ $http({
   method: 'GET',
   url: 'http://localhost:3002/db'
 }).then(function successCallback(response) {
+  // curly brace in line above seems to close out in a commented-out line
   console.log(response);
   // $scope.image = response.data[7].image;
   // for (var i = 0; i < response.data.length; i++) {
@@ -21,7 +22,7 @@ $http({
   // $scope.dataStuff = response.data.items[i].image;
   $scope.products = response.data;
 // }
-  console.log(response.data.items[4].image);
+  // console.log(response.data.items[4].image);
   console.log(response.data.items.length);
   console.log($scope.products = response.data);
   // $scope.dataStuff = {
@@ -32,3 +33,25 @@ $http({
   // };
 });
 });
+
+
+// experimenting below from http://stackoverflow.com/questions/24175119/angularjs-dynamically-populate-details-from-json-based-on-item-selected-------------------------
+// mainApp.controller('MainGet', ['$scope', 'itemsFactory', function($scope, itemsFactory){
+//   var promise = itemsFactory.itemDetails();
+//
+//     promise.then(function (data) {
+//         $scope.itemDetails = data;
+//         console.log(data);
+//     });
+// }]);
+
+
+// experimental code below from http://paynoattn.com/blog/dev/simple-angularjs-listdetail-view-using-ui-router----------------------------------------
+// It uses ui.router, rather than ng-route, so not sure how to modify code
+
+// angular.module('routerApp', ['ng-router']);.controller('listController', ['$scope', '$http', '$state', '$location', function($scope, $http, $state, $location) {
+//     $http.get('data.json').success(function(data){
+//       $scope.thisAlbum = $state.params.id;
+//       $scope.products = data;
+//     });
+// }]);
